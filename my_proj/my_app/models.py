2bc9f980cf70c3ff2cd593bef37c06eb3520793e
Dmitriy Semenovskiy <dmitriy.semenovskiy@gmail.com>
2019-12-06 18:36:47 +0200

    add Gadget model

--- my_proj/my_app/models.py
+++ my_proj/my_app/models.py
@@ -1,3 +1,13 @@
+from django.contrib.postgres.fields import JSONField, ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Gadget(models.Model):
+	code = models.SlugField(max_length=10)
+	category = models.CharField(max_length=50)
+	link = models.URLField()
+	price = models.PositiveIntegerField()
+	cashback = models.PositiveIntegerField()
+	full_desc = models.TextField()
+	tech = JSONField()
+	photo_links = ArrayField(models.URLField())
