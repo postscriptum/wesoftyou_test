10b7616ad45f67c8d3499823e2ac8a366e9fd921
Dmitriy Semenovskiy <dmitriy.semenovskiy@gmail.com>
2019-12-08 22:53:21 +0200

    implement price, cashback and tech filters

--- my_proj/my_app/views.py
+++ my_proj/my_app/views.py
@@ -4,5 +4,30 @@ from rest_framework import generics
 
 
 class GadgetList(generics.ListAPIView):
-    queryset = Gadget.objects.all()
     serializer_class = GadgetSerializer
+
+    def get_queryset(self):
+        queryset = Gadget.objects.all()
+        # price filter
+        min_price = self.request.query_params.get('min_price', None)
+        if min_price is not None:
+            queryset = queryset.filter(price__gte=int(min_price))
+        max_price = self.request.query_params.get('max_price', None)
+        if max_price is not None:
+            queryset = queryset.filter(price__lte=int(max_price))
+        # cashback filter
+        min_cash = self.request.query_params.get('min_cash', None)
+        if min_cash is not None:
+            queryset = queryset.filter(cashback__gte=int(min_cash))
+        max_cash = self.request.query_params.get('max_cash', None)
+        if max_cash is not None:
+            queryset = queryset.filter(cashback__lte=int(max_cash))
+        # RAM filter
+        ram = self.request.query_params.get('ram', None)
+        if ram is not None:
+            queryset = queryset.filter(tech__contains={'Объем оперативной памяти': '{} Гб'.format(ram)})
+        # cores filter
+        cores = self.request.query_params.get('cores', None)
+        if cores is not None:
+            queryset = queryset.filter(tech__contains={'Количество ядер процессора': '{}'.format(cores)})
+        return queryset
